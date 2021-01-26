4ad8e23d9d620a7c232d7ce6ff87f00b0033361a
Sohrat <sohrat6128@gmail.com>
2021-01-26 09:31:16 +0300

    set validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,24 +1,49 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
-const Task = mongoose.model("Task", {
-  description: String,
-  completed: Boolean,
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowerCase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Not a valid email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 6,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error("not a valid password!");
+      }
+    },
+  },
 });
 
-const task1 = new Task({
-  description: "My test",
-  completed: false,
+const usmurtier = new User({
+  name: "shoki",
+  password: "Mynewpassword",
+  email: "   murti@gmail.com  ",
 });
 
-task1
+usmurtier
   .save()
   .then(() => {
-    console.log(task1);
+    console.log(usmurtier);
   })
   .catch((error) => {
     console.log(error);
