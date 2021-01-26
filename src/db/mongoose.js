bdcdd7025fa3ec21fdeb24279ebadcc52430df3e
Sohrat <sohrat6128@gmail.com>
2021-01-26 19:49:00 +0300

    create user model and set new user

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,50 +1,8 @@
 const mongoose = require("mongoose");
-const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
   useCreateIndex: true,
 });
 
-const User = mongoose.model("User", {
-  name: {
-    type: String,
-  },
-  email: {
-    type: String,
-    required: true,
-    trim: true,
-    lowerCase: true,
-    validate(value) {
-      if (!validator.isEmail(value)) {
-        throw new Error("Not a valid email");
-      }
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
-    minLength: 6,
-    validate(value) {
-      if (value.includes("password")) {
-        throw new Error("not a valid password!");
-      }
-    },
-  },
-});
 
-const usmurtier = new User({
-  name: "shoki",
-  password: "Mynewpassword",
-  email: "   murti@gmail.com  ",
-});
-
-usmurtier
-  .save()
-  .then(() => {
-    console.log(usmurtier);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
---
+++ src/models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const User = mongoose.model("User", {
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowerCase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Not a valid email");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minLength: 7,
+    validate(value) {
+      if (value.includes("password")) {
+        throw new Error("not a valid password!");
+      }
+    },
+  },
+  age: {
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a postive number");
+      }
+    },
+  },
+});
+
+module.exports = User;
