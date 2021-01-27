c27d9e2da4a3b7e445160004598d8c76d093eedb
Sohrat <sohrat6128@gmail.com>
2021-01-27 17:40:56 +0300

    create task router

--- src/index.js
+++ src/index.js
@@ -1,83 +1,15 @@
 const express = require("express");
 require("./db/mongoose");
 
-const Task = require("./models/task");
 const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
-
-app.post("/task", async (req, res) => {
-  const task = new Task(req.body);
-
-  try {
-    await task.save();
-    res.status(201).send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.patch("/tasks/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["description", "completed"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-  if (!isValidOperation) {
-    return res.status(404).send();
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.get("/tasks", async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-    return res.send(tasks);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/tasks/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const task = await Task.findById(_id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.delete("/tasks/:id", async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-    if (!task) {
-      return res.status(404).send();
-    }
-    res.send(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
