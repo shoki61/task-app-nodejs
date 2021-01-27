b48fd442422277f86ccde47383ea2a1055a1a803
Sohrat <sohrat6128@gmail.com>
2021-01-27 17:43:07 +0300

    fix

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const Task = require("../models/task");
-const router = express.Router();
+const router = new express.Router();
 
 router.post("/task", async (req, res) => {
   const task = new Task(req.body);
