595e5c920afc7e510c1cb48f90f63fe06888b60c
Sohrat <sohrat6128@gmail.com>
2021-01-27 17:34:15 +0300

    create routers file and carry the users in it

--- src/index.js
+++ src/index.js
@@ -1,47 +1,14 @@
 const express = require("express");
 require("./db/mongoose");
 
-const User = require("./models/user");
 const Task = require("./models/task");
+const userRouter = require("./routers/user");
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  const user = new User(req.body);
-  try {
-    await user.save();
-    res.status(201).send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-
-app.patch("/users/:id", async (req, res) => {
-  const updates = Object.keys(req.body);
-  const allowedUpdates = ["name", "email", "password", "age"];
-  const isValidOperation = updates.every((update) =>
-    allowedUpdates.includes(update)
-  );
-  if (!isValidOperation) {
-    return res.status(404).send();
-  }
-
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
+app.use(userRouter);
 
 app.post("/task", async (req, res) => {
   const task = new Task(req.body);
@@ -78,28 +45,6 @@ app.patch("/tasks/:id", async (req, res) => {
   }
 });
 
-app.get("/users", async (req, res) => {
-  try {
-    const users = await User.find({});
-    res.send(users);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.get("/users/:id", async (req, res) => {
-  const _id = req.params.id;
-  try {
-    const user = await User.findById(_id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
 app.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find({});
@@ -117,18 +62,6 @@ app.get("/tasks/:id", async (req, res) => {
       return res.status(404).send();
     }
     res.send(task);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-
-app.delete("/users/:id", async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-    if (!user) {
-      return res.status(404).send();
-    }
-    res.send(user);
   } catch (error) {
     res.status(500).send(error);
   }
