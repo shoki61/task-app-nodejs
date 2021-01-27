2ec3268241b45bca4bf32f52640ba0a5b72ed6c4
Sohrat <sohrat6128@gmail.com>
2021-01-27 13:54:00 +0300

    set async fonction

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,7 +2,7 @@ require("../src/db/mongoose");
 
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("600ee137f1458b1224ce1dcc", { age: 1 })
+/*User.findByIdAndUpdate("600ee137f1458b1224ce1dcc", { age: 1 })
   .then((user) => {
     console.log(user);
     return User.countDocuments({ age: 1 });
@@ -10,6 +10,18 @@ User.findByIdAndUpdate("600ee137f1458b1224ce1dcc", { age: 1 })
   .then((result) => {
     console.log(result);
   })
+  .catch((error) => {
+    console.log(error);
+  });*/
+
+const updateAgeAndCount = async (id, age) => {
+  const user = await User.findByIdAndUpdate(id, { age });
+  const count = await User.countDocuments({ age });
+  return count;
+};
+
+updateAgeAndCount("600ee137f1458b1224ce1dcc", 125)
+  .then((count) => console.log(count))
   .catch((error) => {
     console.log(error);
   });
