a1324e41cbcb4e02c48e36a1b82b6ab2e8622a40
Sohrat <sohrat6128@gmail.com>
2021-01-27 10:08:36 +0300

    fetch task by id

--- src/index.js
+++ src/index.js
@@ -69,6 +69,20 @@ app.get("/tasks", (req, res) => {
     });
 });
 
+app.get("/tasks/:id", (req, res) => {
+  const _id = req.params.id;
+  Task.findById(_id)
+    .then((task) => {
+      if (!task) {
+        return res.status(404).send();
+      }
+      res.send(task);
+    })
+    .catch((error) => {
+      res.status(500).send(error);
+    });
+});
+
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
