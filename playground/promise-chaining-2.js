0a11a7c78e98312da656538098d94433b5ec6c36
Sohrat <sohrat6128@gmail.com>
2021-01-28 10:19:04 +0300

    securely storing passeords useing the bcrypsjs

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -2,18 +2,6 @@ require("../src/db/mongoose");
 
 const Task = require("../src/models/task");
 
-/*Task.findByIdAndDelete("60104a286a14bd4bd00f9cf0")
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });*/
-
 const deleteTaskAndCount = async (id) => {
   const task = await Task.findByIdAndDelete(id);
   const count = await Task.countDocuments({ completed: false });
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -2,18 +2,6 @@ require("../src/db/mongoose");
 
 const User = require("../src/models/user");
 
-/*User.findByIdAndUpdate("600ee137f1458b1224ce1dcc", { age: 1 })
-  .then((user) => {
-    console.log(user);
-    return User.countDocuments({ age: 1 });
-  })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.log(error);
-  });*/
-
 const updateAgeAndCount = async (id, age) => {
   const user = await User.findByIdAndUpdate(id, { age });
   const count = await User.countDocuments({ age });
