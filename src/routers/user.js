369279c1d42e784bb73ab690e0b58bb18b225929
Sohrat <sohrat6128@gmail.com>
2021-01-28 16:48:43 +0300

    create auth middleware

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,13 +2,14 @@ const express = require("express");
 
 const User = require("../models/user");
 const router = new express.Router();
+const auth = require("../middleware/auth");
 
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
     await user.save();
     const token = await user.generateAuthToken();
-    res.status(201).send({user, token});
+    res.status(201).send({ user, token });
   } catch (error) {
     res.status(400).send(error);
   }
@@ -51,6 +52,10 @@ router.patch("/users/:id", async (req, res) => {
   } catch (error) {
     res.status(400).send(error);
   }
+});
+
+router.get("/users/profile", auth, async (req, res) => {
+  res.send(req.user);
 });
 
 router.get("/users", async (req, res) => {
