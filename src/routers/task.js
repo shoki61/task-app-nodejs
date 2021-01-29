6c8df0177f1ad92384f1d38742c4dc932220c8dc
Sohrat <sohrat6128@gmail.com>
2021-01-29 11:35:23 +0300

    set middleware to task router

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,7 +19,7 @@ router.post("/task",auth, async (req, res) => {
   }
 });
 
-router.patch("/tasks/:id", async (req, res) => {
+router.patch("/tasks/:id", auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ["description", "completed"];
   const isValidOperation = updates.every((update) =>
@@ -30,36 +30,33 @@ router.patch("/tasks/:id", async (req, res) => {
   }
 
   try {
-    const task = await Task.findById(req.params.id);
-
-    updates.forEach((update) => {
-      task[update] = req.body[update];
-    });
-
-    await task.save();
+    const task = await Task.findOne({_id:req.params.id, owner: req.user._id});
 
     if (!task) {
       return res.status(404).send();
     }
+
+    updates.forEach( update => task[update] = req.body[update]);
+    await task.save();
     res.send(task);
   } catch (error) {
     res.status(400).send(error);
   };
 });
 
-router.get("/tasks", async (req, res) => {
+router.get("/tasks", auth, async (req, res) => {
   try {
-    const tasks = await Task.find({});
-    return res.send(tasks);
+    const tasks = await Task.find({owner: req.user._id});
+    res.send(tasks);
   } catch (error) {
     res.status(500).send(error);
   }
 });
 
-router.get("/tasks/:id", async (req, res) => {
+router.get("/tasks/:id",auth, async (req, res) => {
   const _id = req.params.id;
   try {
-    const task = await Task.findById(_id);
+    const task = await Task.findOne({_id, owner: req.user._id});
     if (!task) {
       return res.status(404).send();
     }
@@ -69,9 +66,9 @@ router.get("/tasks/:id", async (req, res) => {
   }
 });
 
-router.delete("/tasks/:id", async (req, res) => {
+router.delete("/tasks/:id", auth, async (req, res) => {
   try {
-    const task = await Task.findByIdAndDelete(req.params.id);
+    const task = await Task.findOneAndDelete({_id:req.params.id, owner: req.user._id});
     if (!task) {
       return res.status(404).send();
     }
