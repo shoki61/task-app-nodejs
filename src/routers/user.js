62c018217cb59246da2add18df0cf2df643996c6
Sohrat <sohrat6128@gmail.com>
2021-02-01 11:11:21 +0300

    allow image upload only

--- src/routers/user.js
+++ src/routers/user.js
@@ -113,8 +113,18 @@ router.delete("/users/profile",auth, async (req, res) => {
 });
 
 const upload = multer({
-  dest:'avatars'
-})
+  dest:'avatars',
+  limits:{
+    fileSize: 1000000,
+  },
+  fileFilter(req,file,cb){
+    if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+      return cb(new Error('Please upload an image'));
+    };
+
+    cb(undefined, true);
+  }
+});
 
 router.post('/users/profile/avatar',upload.single('avatar'), async (req, res)=>{
   try{
