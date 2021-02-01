9d7e61d2fb1e124d08b130e629a27c98187e4894
Sohrat <sohrat6128@gmail.com>
2021-02-01 13:53:49 +0300

    Save the image using sharp

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const multer = require('multer');
+const sharp = require('sharp');
 
 const User = require("../models/user");
 const router = new express.Router();
@@ -126,7 +127,8 @@ const upload = multer({
 });
 
 router.post('/users/profile/avatar', auth, upload.single('avatar'), async (req, res)=>{
-  req.user.avatar = req.file.buffer;
+  const buffer = await sharp(req.file.buffer).resize({width:250, height:250}).png().toBuffer();
+  req.user.avatar = buffer;
   await req.user.save();
   res.send();
 },(error, req, res, next) => {
@@ -134,6 +136,7 @@ router.post('/users/profile/avatar', auth, upload.single('avatar'), async (req, 
 });
 
 router.delete('/users/profile/avatar', auth, async(req, res)=>{
+
   req.user.avatar = undefined;
   await req.user.save();
   res.send();
@@ -146,7 +149,7 @@ router.get('/users/:id/avatar', async(req, res)=>{
     if(!user || !user.avatar){
       throw new Error();
     };
-    res.set('Content-Type','image/jpg');
+    res.set('Content-Type','image/png');
     res.send(user.avatar);
   }catch(error){
     res.status(404).send(error);
