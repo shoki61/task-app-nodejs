eea83cb4b1aa55261d9f45b39325fa8897a3c8db
Sohrat <sohrat6128@gmail.com>
2021-02-01 16:50:39 +0300

    create welcome email

--- src/emails/account.js
+++ src/emails/account.js
@@ -4,18 +4,16 @@ const sendgridAPIKey = require('../env');
 
 sgMail.setApiKey(sendgridAPIKey);
 
-const msg = {
-    to: 'orazmuradik@gmail.com', // Change to your recipient
-    from: 'sohrat6128@hotmail.com', // Change to your verified sender
-    subject: 'Sending with SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-  }
-sgMail
-    .send(msg)
-    .then(() => {
-      console.log('Email sent')
-    })
-    .catch((error) => {
-      console.error(error)
-    })
+const sendWelcomeEmail = (email, name) =>{
+    sgMail.send({
+        to: email,
+        from: 'sohrat6128@hotmail.com',
+        subject:'Thanks for joining in!',
+        text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+
+    });
+};
+
+module.exports = {
+    sendWelcomeEmail
+};
